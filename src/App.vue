d39dcced26a11a4cd752ae3a2eb3a635d7588edc
Antonija Zukina <azukina@tvz.hr>
2022-01-16 17:51:41 +0100

    Router

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,21 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <router-link to="/">Body</router-link> | 
+     <router-link to="/about">Header</router-link> 
     </div>
     <router-view/>
   </div>
 </template>
 
+<script>
+import Header from './views/Header.vue';
+import Body from './views/Body.vue';
+import Footer from './views/Footer.vue';
+</script>
+
+
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
