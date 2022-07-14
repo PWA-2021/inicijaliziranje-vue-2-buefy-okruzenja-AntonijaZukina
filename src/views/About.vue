82a318245ce5f9632765816eafb45ee9e13c4c09
Antonija Zukina <72887417+AntonijaZukina@users.noreply.github.com>
2022-07-14 22:55:24 +0200

    about + slikice

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,78 +1,56 @@
 <template>
-  <div class="cont">
-    <div class="intro">
-      <h1>Useful Tools For Your Life</h1>
-      <p>
-        Our platform is used for help people to reach their success day by day
-      </p>
-    </div>
-    <section class="row-3">
-      <div class="first">
-        <img src="img/calender.png" />
-        <h2>Daily Based</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nisi
-          lacus, ullamcorper vitae varius eu, bibendum eu nulla. Nunc
-          pellentesque leo quis diam dictum tincidunt. Interdum et malesuada
-          fames ac ante ipsum primis in faucibus. Aliquam aliquet volutpat ipsum
-          vitae dictum. Mauris ultricies, quam sit amet laoreet ullamcorper,
-          magna augue lobortis quam, vel pharetra sem erat a risus. Duis luctus
-          maximus justo, nec maximus neque scelerisque feugiat. Mauris gravida
-          ornare sem eleifend interdum. Donec vitae massa quam. Etiam pulvinar
-          velit metus, at pellentesque purus condimentum in. Nam nec lacus et
-          turpis auctor pretium. Suspendisse sodales mauris in egestas
-          vulputate. Aliquam erat volutpat. Nam in tortor ante. Sed eu risus
-          eget magna tempus mattis.
-        </p>
-        <a href="#">read more</a>
-      </div>
-      <div class="second">
-        <img src="img/clock.png" />
-        <h2>Timeline & Milestone</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nisi
-          lacus, ullamcorper vitae varius eu, bibendum eu nulla. Nunc
-          pellentesque leo quis diam dictum tincidunt. Interdum et malesuada
-          fames ac ante ipsum primis in faucibus. Aliquam aliquet volutpat ipsum
-          vitae dictum. Mauris ultricies, quam sit amet laoreet ullamcorper,
-          magna augue lobortis quam, vel pharetra sem erat a risus. Duis luctus
-          maximus justo, nec maximus neque scelerisque feugiat. Mauris gravida
-          ornare sem eleifend interdum. Donec vitae massa quam. Etiam pulvinar
-          velit metus, at pellentesque purus condimentum in. Nam nec lacus et
-          turpis auctor pretium. Suspendisse sodales mauris in egestas
-          vulputate. Aliquam erat volutpat. Nam in tortor ante. Sed eu risus
-          eget magna tempus mattis.
-        </p>
-        <a href="#">read more</a>
-      </div>
-      <div class="third">
-        <img src="img/user.png" />
-        <h2>Community Support</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nisi
-          lacus, ullamcorper vitae varius eu, bibendum eu nulla. Nunc
-          pellentesque leo quis diam dictum tincidunt. Interdum et malesuada
-          fames ac ante ipsum primis in faucibus. Aliquam aliquet volutpat ipsum
-          vitae dictum. Mauris ultricies, quam sit amet laoreet ullamcorper,
-          magna augue lobortis quam, vel pharetra sem erat a risus. Duis luctus
-          maximus justo, nec maximus neque scelerisque feugiat. Mauris gravida
-          ornare sem eleifend interdum. Donec vitae massa quam. Etiam pulvinar
-          velit metus, at pellentesque purus condimentum in. Nam nec lacus et
-          turpis auctor pretium. Suspendisse sodales mauris in egestas
-          vulputate. Aliquam erat volutpat. Nam in tortor ante. Sed eu risus
-          eget magna tempus mattis.
-        </p>
-        <a href="#">read more</a>
-      </div>
-    </section>
-  </div>
+  <div class ="body">        
+        <nav>
+            <ul>
+                <li><a href="#">Početna</a></li>
+                <li><a href="#">O nama</a></li>
+                <li><a href="#">Naši instruktori</a></li>
+                <li><a href="#">Kontakt</a></li>
+            </ul>
+        </nav>
+        
+        <div class="cont">
+            <div class="intro">
+                <h1>Ono što mi vjerujemo </h1>
+                <p>Tu smo kako bi svi zavoljeli učenje i kako bi svakom pristupili najbolje što znamo!</p>
+            </div>
+            <section class="row-3">
+                <div class="first">
+                    <img src="~@/assets/calender.png" />
+                    <h2>Raspored</h2>
+                    <p>Mišljenje je kompleksan psihološki proces, koji zbog složenosti nije u potpunosti proučen. U svakodnevnom govoru mišljenje nije jednoznačan pojam: kada nekoga potičemo da se sjeti nečega (tj. da reproducira zapamćeni materijal), često mu kažemo misli malo, npr. kad zimi mislimo o tome kako bi nam bilo lijepo na nekoj toploj plaži, onda su to zapravo predodžbe mašte, a ako nekoga upitamo što misliš o religiji, mi zapravo želimo čuti njegovo stajalište o tome. Mišljenje uključuje nenamjerno ili namjerno služenje različitim kognitivnim (spoznajnim) doživljajima (osjeti, percepcije, predodžbe, pamćenja, riječi, pojmovi, stavovi itd.), što sve zajedno čini ono što se zove naša svijest o sebi i zbivanjima oko sebe. </p>
+                    <a href="#">saznaj više</a>
+                </div>
+                <div class="second">
+                    <img src="~@/assets/clock.png" />
+                    <h2>Vrijeme</h2>
+                    <p>Logika je umijeće ispravnoga mišljenja, rasuđivanja, prosuđivanja, raspravljanja. Temeljna je filozofska disciplina koja se bavi ispravnim oblicima mišljenja, poimanja, suđenja i zaključivanja, te je stoga polazište i nužni temelj svake valjane spoznaje. U širem smislu mogu se razlikovati filozofijska i znanstvena logika: predmet prve jest izučavanje misli kao zasebne realnosti te kritika i ispitivanje mogućnosti mišljenja uopće, pa se izravnije veže uz probleme spoznajne teorije; druga ispituje mogućnost i primjerenost posebnih misaonih stavova (stajališta), tvrdnji i pravila po kojima je ustrojen misaoni iskaz. Premda stalno mislimo (kada smo pri svijesti), proces mišljenja nije nam introspektivno dovoljno dostupan.</p>
+                    <a href="#">saznaj više</a>
+                </div>
+                <div class="third">
+                    <img src="~@/assets/user.png" />
+                    <h2>Razmišljanje</h2>
+                    <p>Razmišljanje nije prikupljanje informacija niti analiza podataka. Ono nije samo povezivanje predmeta s konceptima odnosno uspostavljanje logičkih veza između koncepata. Razmišljanje uključuje nešto više od mentalne procedure. Razmišljanje je stalna potraga za odgovorom na krajnje pitanje zašto nešto jest, radije negoli ništa. Budući da ništa ne izmiče egzistenciji te da ništa nije izvan velikog lanca bića, cjelokupno razmišljanje jeste aktivnost unutar egzistencije i njenih beskrajnih modaliteta. Bilo da sprovodimo neki naučni eksperiment, pišemo lijepu pjesmu na jutarnjem povjetarcu ili analiziramo moderne gradove, mi se bavimo različitim aspektima i manifestacijama egzistencije.</p>
+                    <a href="#">saznaj više</a>
+                </div>
+            </section>
+        </div>
 </template>
 
+<script>
+export default {
+  name: "About",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
 <style scoped>
-body {
-  background-image: url(img/hero-bg.jpg);
+.body {
+  background: url("assets/hero-bg.jpg");
   background-size: cover;
-  font-family: verdana;
+  font-family: "Times New Roman", Times, serif;
   font-size: 14px;
   margin: 0;
 }
@@ -167,12 +145,5 @@ section.row-3 div.second {
 
 section.row-3 div.third {
   left: 660px;
-}
-
-footer {
-  background: #fafafa;
-  border-top: 1px solid #d7d7d7;
-  padding: 30px 20px;
-  text-align: center;
 }
 </style>
